50e10204a4112511cb1e0a0529098b8b3faed645
luispuentesvega <luispuentesvega@gmail.com>
2022-09-10 14:25:44 +0700

    feat: implement tailwind and react component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,35 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Swatch from '@uiw/react-color-swatch';
 
 import styles from '../styles/Home.module.css';
 import { ColorService } from '../src/utils/ColorService';
+import { hsvaToHex, getContrastingColor, } from '@uiw/color-convert';
 
 const SERVER_HOST = 'http://localhost:3000/api';
+
+function Point(props: { color?: string; checked?: boolean; }) {
+  if (!props.checked) return null;
+  return (
+    <div
+      style={{
+        height: 5,
+        width: 5,
+        borderRadius: '50%',
+        backgroundColor: getContrastingColor(props.color!),
+      }}
+    />
+  );
+}
+
 
 const Home: NextPage = () => {
   const [colors, setColors] = useState([]);
 
-  const mapColors = () => {
-
-  };
-
+  useEffect(() => {
+    fetchColors();
+  }, []);
 
   const fetchColors = async (): Promise<void> => {
     try {
@@ -35,15 +50,21 @@ const Home: NextPage = () => {
   };
 
   const handleChangeColors = () => {
-    console.log('Onclick');
     fetchColors();
   };
 
   return (
-    <div className={styles.container}>
-      <h1>Color Swatch</h1>
-      <Swatch colors={colors} />
-      <button onClick={handleChangeColors}>Update Colors</button>
+    <div className="flex flex-row min-h-screen justify-center items-center bg-slate-100">
+      <div className="bg-orange-300 px-8 py-8 rounded-lg">
+        <h1 className="text-3xl font-bold text-violet-500 mb-10">Color Swatch</h1>
+        <Swatch colors={colors}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }} className="mb-2 text-center" />
+        <button onClick={handleChangeColors} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full mt-10">Refresh</button>
+      </div>
     </div>
   );
 };
