6825eccb493268015177d5c56d9f472b921e710a
luispuentesvega <luispuentesvega@gmail.com>
2022-09-13 00:57:53 +0700

    add nojeckyll

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/nextjs-pages',
-  assetPrefix: '/nextjs-pages',
+  basePath: '/colors',
+  assetPrefix: '/colors',
 };
 
 module.exports = nextConfig;
